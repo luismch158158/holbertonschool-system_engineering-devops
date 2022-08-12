76aca655b40f6147180a2e1afdc67c5202e56b66
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-12 12:19:18 -0500

    Fixing puppet lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 #!/usr/bin/pup
 #Using Puppet, create a manifest that kills a process named killmenow.
 exec {'pkill':
-  command   => 'pkill killmenow',
-  user      => 'root',
-  provider  => 'shell',
-  path      => '/usr/local/bin/:/bin:/usr/sbin',
+  command  =>  'pkill killmenow',
+  user     =>  'root',
+  provider =>  'shell',
+  path     =>  '/usr/local/bin/:/bin:/usr/sbin',
 }
