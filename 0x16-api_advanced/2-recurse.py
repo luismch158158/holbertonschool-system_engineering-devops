35c8633eb9c62da45bffc208a0396e0cfebe6643
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-09-27 12:54:49 -0500

    Fix some errors

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,22 +18,19 @@ def recurse(subreddit, hot_list=[]):
     '''
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-
     response = requests.get(url, headers=header, allow_redirects=False)
 
-    if response.status_code != 200:
+    if (response.status_code != 200):
         return (None)
 
     len_hotlist = len(hot_list)
 
     data = response.json().get('data')
     array_elements = data.get('children')
+
     if (len_hotlist != len(array_elements)):
         new_element = array_elements[len_hotlist].get('data').get('title')
         hot_list.append(new_element)
-        # print()
-        # print(hot_list)
-        # print()
         recurse(subreddit, hot_list)
 
     return hot_list
